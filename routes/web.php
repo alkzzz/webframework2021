509871abe633c9c4da172f5fa2bc91f8240c9e19
alkzzz <m.alkaff9@gmail.com>
2021-09-01 16:29:17 +0800

    menambahkan template bootstrap untuk halaman utama

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\NewsController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/news', [NewsController::class, 'index']);
